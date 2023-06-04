7ec17727198a3f8bd44963019e3ac7cc24c8c703
Ilia Lazarev <ilia.lazarev@icloud.com>
2023-06-04 12:25:26 +0300

    3.x (#10)
    * updated readme
    * optional dto params

--- src/SteamUserDto.php
+++ src/SteamUserDto.php
@@ -8,21 +8,21 @@ final class SteamUserDto
 {
     private function __construct(
         private readonly string $steamId,
-        private readonly int $communityVisibilityState,
-        private readonly int $profileState,
-        private readonly string $personaName,
-        private readonly int $commentPermission,
-        private readonly string $profileUrl,
-        private readonly string $avatar,
-        private readonly string $avatarMedium,
-        private readonly string $avatarFull,
-        private readonly string $avatarHash,
-        private readonly int $lastLogoff,
-        private readonly int $personaState,
-        private readonly string $primaryClanId,
-        private readonly int $timeCreated,
-        private readonly int $personaStateFlags,
-        private readonly string $locCountryCode,
+        private readonly ?int $communityVisibilityState,
+        private readonly ?int $profileState,
+        private readonly ?string $personaName,
+        private readonly ?int $commentPermission,
+        private readonly ?string $profileUrl,
+        private readonly ?string $avatar,
+        private readonly ?string $avatarMedium,
+        private readonly ?string $avatarFull,
+        private readonly ?string $avatarHash,
+        private readonly ?int $lastLogoff,
+        private readonly ?int $personaState,
+        private readonly ?string $primaryClanId,
+        private readonly ?int $timeCreated,
+        private readonly ?int $personaStateFlags,
+        private readonly ?string $locCountryCode,
         private readonly ?int $playerLevel,
     ) {
     }
@@ -31,21 +31,21 @@ final class SteamUserDto
     {
         return new self(
             steamId: $data['steamid'],
-            communityVisibilityState: $data['communityvisibilitystate'],
-            profileState: $data['profilestate'],
-            personaName: $data['personaname'],
-            commentPermission: $data['commentpermission'],
-            profileUrl: $data['profileurl'],
-            avatar: $data['avatar'],
-            avatarMedium: $data['avatarmedium'],
-            avatarFull: $data['avatarfull'],
-            avatarHash: $data['avatarhash'],
-            lastLogoff: $data['lastlogoff'],
-            personaState: $data['personastate'],
-            primaryClanId: $data['primaryclanid'],
-            timeCreated: $data['timecreated'],
-            personaStateFlags: $data['personastateflags'],
-            locCountryCode: $data['loccountrycode'],
+            communityVisibilityState: $data['communityvisibilitystate'] ?? null,
+            profileState: $data['profilestate'] ?? null,
+            personaName: $data['personaname'] ?? null,
+            commentPermission: $data['commentpermission'] ?? null,
+            profileUrl: $data['profileurl'] ?? null,
+            avatar: $data['avatar'] ?? null,
+            avatarMedium: $data['avatarmedium'] ?? null,
+            avatarFull: $data['avatarfull'] ?? null,
+            avatarHash: $data['avatarhash'] ?? null,
+            lastLogoff: $data['lastlogoff'] ?? null,
+            personaState: $data['personastate'] ?? null,
+            primaryClanId: $data['primaryclanid'] ?? null,
+            timeCreated: $data['timecreated'] ?? null,
+            personaStateFlags: $data['personastateflags'] ?? null,
+            locCountryCode: $data['loccountrycode'] ?? null,
             playerLevel: $data['player_level'] ?? null,
         );
     }
@@ -55,77 +55,77 @@ final class SteamUserDto
         return $this->steamId;
     }
 
-    public function getCommunityVisibilityState(): int
+    public function getCommunityVisibilityState(): ?int
     {
         return $this->communityVisibilityState;
     }
 
-    public function getProfileState(): int
+    public function getProfileState(): ?int
     {
         return $this->profileState;
     }
 
-    public function getPersonaName(): string
+    public function getPersonaName(): ?string
     {
         return $this->personaName;
     }
 
-    public function getCommentPermission(): int
+    public function getCommentPermission(): ?int
     {
         return $this->commentPermission;
     }
 
-    public function getProfileUrl(): string
+    public function getProfileUrl(): ?string
     {
         return $this->profileUrl;
     }
 
-    public function getAvatar(): string
+    public function getAvatar(): ?string
     {
         return $this->avatar;
     }
 
-    public function getAvatarMedium(): string
+    public function getAvatarMedium(): ?string
     {
         return $this->avatarMedium;
     }
 
-    public function getAvatarFull(): string
+    public function getAvatarFull(): ?string
     {
         return $this->avatarFull;
     }
 
-    public function getAvatarHash(): string
+    public function getAvatarHash(): ?string
     {
         return $this->avatarHash;
     }
 
-    public function getLastLogoff(): int
+    public function getLastLogoff(): ?int
     {
         return $this->lastLogoff;
     }
 
-    public function getPersonaState(): int
+    public function getPersonaState(): ?int
     {
         return $this->personaState;
     }
 
-    public function getPrimaryClanId(): string
+    public function getPrimaryClanId(): ?string
     {
         return $this->primaryClanId;
     }
 
-    public function getTimeCreated(): int
+    public function getTimeCreated(): ?int
     {
         return $this->timeCreated;
     }
 
-    public function getPersonaStateFlags(): int
+    public function getPersonaStateFlags(): ?int
     {
         return $this->personaStateFlags;
     }
 
-    public function getLocCountryCode(): string
+    public function getLocCountryCode(): ?string
     {
         return $this->locCountryCode;
     }
